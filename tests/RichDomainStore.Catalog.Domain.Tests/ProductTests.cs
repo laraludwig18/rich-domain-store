3ef5db3e0bce717b3aa189739034b5ff44ad4450
Lara Ludwig <laraludwig18@gmail.com>
2021-10-24 18:50:17 -0300

    fix: remove registerDate parameter in product test

--- tests/RichDomainStore.Catalog.Domain.Tests/ProductTests.cs
+++ tests/RichDomainStore.Catalog.Domain.Tests/ProductTests.cs
@@ -11,7 +11,7 @@ namespace RichDomainStore.Catalog.Domain.Tests
         public void Product_Validate_EmptyProductNameShouldThrowException()
         {
             var ex = Assert.Throws<DomainException>(() =>
-                new Product(string.Empty, "Description", false, 100, Guid.NewGuid(), DateTime.Now, "Image", new Dimensions(1, 1, 1))
+                new Product(string.Empty, "Description", false, 100, Guid.NewGuid(), "Image", new Dimensions(1, 1, 1))
             );
 
             Assert.Equal("Product name cannot be empty", ex.Message);
@@ -21,7 +21,7 @@ namespace RichDomainStore.Catalog.Domain.Tests
         public void Product_Validate_EmptyProductDescriptionShouldThrowException()
         {
             var ex = Assert.Throws<DomainException>(() =>
-                new Product("Name", string.Empty, false, 100, Guid.NewGuid(), DateTime.Now, "Image", new Dimensions(1, 1, 1))
+                new Product("Name", string.Empty, false, 100, Guid.NewGuid(), "Image", new Dimensions(1, 1, 1))
             );
 
             Assert.Equal("Product description cannot be empty", ex.Message);
@@ -31,7 +31,7 @@ namespace RichDomainStore.Catalog.Domain.Tests
         public void Product_Validate_ProductValueLessThan1ShouldThrowException()
         {
             var ex = Assert.Throws<DomainException>(() =>
-                new Product("Name", "Description", false, 0, Guid.NewGuid(), DateTime.Now, "Image", new Dimensions(1, 1, 1))
+                new Product("Name", "Description", false, 0, Guid.NewGuid(), "Image", new Dimensions(1, 1, 1))
             );
 
             Assert.Equal("Product value cannot be less than 1", ex.Message);
@@ -41,7 +41,7 @@ namespace RichDomainStore.Catalog.Domain.Tests
         public void Product_Validate_EmptyProductCategoryShouldThrowException()
         {
             var ex = Assert.Throws<DomainException>(() =>
-                new Product("Name", "Description", false, 100, Guid.Empty, DateTime.Now, "Image", new Dimensions(1, 1, 1))
+                new Product("Name", "Description", false, 100, Guid.Empty, "Image", new Dimensions(1, 1, 1))
             );
 
             Assert.Equal("Product category id cannot be empty", ex.Message);
@@ -51,7 +51,7 @@ namespace RichDomainStore.Catalog.Domain.Tests
         public void Product_Validate_EmptyProductImageShouldThrowException()
         {
             var ex = Assert.Throws<DomainException>(() =>
-                new Product("Name", "Description", false, 100, Guid.NewGuid(), DateTime.Now, string.Empty, new Dimensions(1, 1, 1))
+                new Product("Name", "Description", false, 100, Guid.NewGuid(), string.Empty, new Dimensions(1, 1, 1))
             );
 
             Assert.Equal("Product image cannot be empty", ex.Message);
@@ -61,7 +61,7 @@ namespace RichDomainStore.Catalog.Domain.Tests
         public void Product_Validate_HeightLessThan1ShouldThrowException()
         {
             var ex = Assert.Throws<DomainException>(() =>
-                new Product("Name", "Description", false, 100, Guid.NewGuid(), DateTime.Now, "Image", new Dimensions(0, 1, 1))
+                new Product("Name", "Description", false, 100, Guid.NewGuid(), "Image", new Dimensions(0, 1, 1))
             );
 
             Assert.Equal("Height cannot be less than 1", ex.Message);
