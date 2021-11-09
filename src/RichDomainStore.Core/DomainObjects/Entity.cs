eb2dd4dcdc4e54b87897ce6f208408408b408681
Lara Ludwig <laraludwig18@gmail.com>
2021-11-09 15:44:36 -0300

    Add events property to base entity

--- src/RichDomainStore.Core/DomainObjects/Entity.cs
+++ src/RichDomainStore.Core/DomainObjects/Entity.cs
@@ -1,14 +1,34 @@
 using System;
+using System.Collections.Generic;
+using RichDomainStore.Core.Messages;
 
 namespace RichDomainStore.Core.DomainObjects
 {
     public abstract class Entity
     {
         public Guid Id { get; set; }
+        private List<Event> _events;
+        public IReadOnlyCollection<Event> Events => _events?.AsReadOnly();
 
         protected Entity()
         {
             Id = Guid.NewGuid();
+        }
+
+        public void AddEvent(Event e)
+        {
+            _events = _events ?? new List<Event>();
+            _events.Add(e);
+        }
+
+        public void RemoveEvent(Event e)
+        {
+            _events?.Remove(e);
+        }
+
+        public void ClearEvents()
+        {
+            _events?.Clear();
         }
 
         public override bool Equals(object obj)
@@ -23,18 +43,18 @@ namespace RichDomainStore.Core.DomainObjects
 
         public static bool operator ==(Entity a, Entity b)
         {
-           if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
                 return true;
-            
+
             if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
                 return false;
-            
+
             return a.Equals(b);
         }
 
         public static bool operator !=(Entity a, Entity b)
         {
-           return !(a == b);
+            return !(a == b);
         }
 
         public override int GetHashCode()
