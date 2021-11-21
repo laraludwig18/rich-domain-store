7b7a9c18371b5a6637d3c9d987b53693526d95f3
Lara Ludwig <laraludwig18@gmail.com>
2021-11-20 21:00:20 -0300

    Turn DomainEvent in abstract class

--- src/RichDomainStore.Core/Messages/CommonMessages/DomainEvents/DomainEvent.cs
+++ src/RichDomainStore.Core/Messages/CommonMessages/DomainEvents/DomainEvent.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace RichDomainStore.Core.Messages.CommonMessages.DomainEvents
 {
-    public class DomainEvent : Event
+    public abstract class DomainEvent : Event
     {
         public DomainEvent(Guid aggregateId)
         {
