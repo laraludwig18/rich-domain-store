5e4c351cdb1066088a363945e2d9281839b1c4d3
Lara Ludwig <laraludwig18@gmail.com>
2021-11-28 17:38:51 -0300

    Add order payment status event handlers

---
+++ src/RichDomainStore.Sales.Application/Events/OrderPaymentDeniedEventHandler.cs
@@ -0,0 +1,25 @@
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using RichDomainStore.Core.Communication.Mediator;
+using RichDomainStore.Core.Messages.CommonMessages.IntegrationEvents;
+using RichDomainStore.Sales.Application.Commands;
+
+namespace RichDomainStore.Sales.Application.Events
+{
+    public class OrderPaymentDeniedEventHandler : INotificationHandler<OrderPaymentDeniedEvent>
+    {
+        private readonly IMediatorHandler _mediatorHandler;
+
+        public OrderPaymentDeniedEventHandler(IMediatorHandler mediatorHandler)
+        {
+            _mediatorHandler = mediatorHandler;
+        }
+
+        public async Task Handle(OrderPaymentDeniedEvent message, CancellationToken cancellationToken)
+        {
+            await _mediatorHandler.SendCommandAsync(new CancelOrderProcessReversingStockCommand(message.OrderId, message.CustomerId))
+                .ConfigureAwait(false);
+        }
+    }
+}
---
+++ src/RichDomainStore.Sales.Application/Events/OrderPaymentPerformedEventHandler.cs
@@ -0,0 +1,25 @@
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using RichDomainStore.Core.Communication.Mediator;
+using RichDomainStore.Core.Messages.CommonMessages.IntegrationEvents;
+using RichDomainStore.Sales.Application.Commands;
+
+namespace RichDomainStore.Sales.Application.Events
+{
+    public class OrderPaymentPerformedEventHandler : INotificationHandler<OrderPaymentPerformedEvent>
+    {
+        private readonly IMediatorHandler _mediatorHandler;
+
+        public OrderPaymentPerformedEventHandler(IMediatorHandler mediatorHandler)
+        {
+            _mediatorHandler = mediatorHandler;
+        }
+
+        public async Task Handle(OrderPaymentPerformedEvent message, CancellationToken cancellationToken)
+        {
+            await _mediatorHandler.SendCommandAsync(new FinishOrderCommand(message.OrderId, message.CustomerId))
+                .ConfigureAwait(false);
+        }
+    }
+}
