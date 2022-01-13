12e0a4bdb8140edfc7785669bdfbf6e739c2a068
Lara Ludwig <laraludwig18@gmail.com>
2022-01-13 16:32:42 -0300

    Add stock service unit tests

---
+++ tests/RichDomainStore.Catalog.Domain.Tests/Services/StockServiceTests.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Threading.Tasks;
+using Moq;
+using Moq.AutoMock;
+using RichDomainStore.Catalog.Domain.Entities;
+using RichDomainStore.Catalog.Domain.Events;
+using RichDomainStore.Catalog.Domain.Interfaces;
+using RichDomainStore.Catalog.Domain.Services;
+using RichDomainStore.Catalog.Domain.Tests.Fixtures;
+using RichDomainStore.Core.Communication.Mediator;
+using Xunit;
+
+namespace RichDomainStore.Catalog.Domain.Tests.Services
+{
+    [Collection(nameof(ProductCollection))]
+    public class StockServiceTests
+    {
+        private readonly ProductFixture _productFixture;
+        private readonly AutoMocker _mocker;
+        private readonly StockService _stockService;
+        private readonly Product _validProduct;
+        public StockServiceTests(ProductFixture productFixture)
+        {
+            _mocker = new AutoMocker();
+            _stockService = _mocker.CreateInstance<StockService>();
+
+            _mocker.GetMock<IProductRepository>()
+             .Setup(r => r.UnitOfWork.CommitAsync())
+             .ReturnsAsync(true);
+
+            _productFixture = productFixture;
+            _validProduct = _productFixture.GenerateValidProduct();
+        }
+
+        [Fact]
+        public async Task DebitStockAsync_ProductWithStockGreatherThan10_ShouldDebitStock()
+        {
+            // Arrange
+            _validProduct.ReStock(11);
+
+            _mocker.GetMock<IProductRepository>()
+             .Setup(r => r.GetByIdAsync(It.IsAny<Guid>()))
+             .ReturnsAsync(_validProduct);
+
+            // Act
+            var success = await _stockService.DebitStockAsync(_validProduct.Id, 1).ConfigureAwait(false);
+
+            // Assert
+            Assert.True(success);
+            _mocker.GetMock<IProductRepository>().Verify(r => r.Update(_validProduct), Times.Once);
+            _mocker.GetMock<IProductRepository>().Verify(r => r.UnitOfWork.CommitAsync(), Times.Once);
+        }
+
+        [Fact]
+        public async Task DebitStockAsync_ProductWithStockLessThan10_ShouldPublishLowProductStockEvent()
+        {
+            // Arrange
+            _validProduct.ReStock(10);
+
+            _mocker.GetMock<IProductRepository>()
+             .Setup(r => r.GetByIdAsync(It.IsAny<Guid>()))
+             .ReturnsAsync(_validProduct);
+
+            // Act
+            var success = await _stockService.DebitStockAsync(_validProduct.Id, 1).ConfigureAwait(false);
+
+            // Assert
+            Assert.True(success);
+            _mocker.GetMock<IMediatorHandler>().Verify(m =>
+                m.PublishDomainEventAsync(It.IsAny<LowProductInStockEvent>()), Times.Once);
+        }
+    }
+}
