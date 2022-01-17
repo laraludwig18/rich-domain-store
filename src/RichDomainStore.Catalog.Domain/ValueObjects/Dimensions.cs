0aa64134fbe8731f2bf4cee556b9fe50a367de63
Lara Ludwig <laraludwig18@gmail.com>
2022-01-17 18:23:42 -0300

    Add min values to const

--- src/RichDomainStore.Catalog.Domain/ValueObjects/Dimensions.cs
+++ src/RichDomainStore.Catalog.Domain/ValueObjects/Dimensions.cs
@@ -4,29 +4,32 @@ namespace RichDomainStore.Catalog.Domain
 {
     public class Dimensions
     {
+        public const decimal MinHeight = 1;
+        public const decimal MinWidth = 1;
+        public const decimal MinDepth = 1;
         public decimal Height { get; private set; }
         public decimal Width { get; private set; }
         public decimal Depth { get; private set; }
 
         public Dimensions(decimal height, decimal width, decimal depth)
         {
-            AssertionConcern.AssertArgumentGreaterThan(height, 1, "Height cannot be less than 1");
-            AssertionConcern.AssertArgumentGreaterThan(width, 1, "Width cannot be less than 1");
-            AssertionConcern.AssertArgumentGreaterThan(depth, 1, "Depth cannot be less than 1");
+            AssertionConcern.AssertArgumentGreaterThan(height, MinHeight, $"Height cannot be less than {MinHeight}");
+            AssertionConcern.AssertArgumentGreaterThan(width, MinWidth, $"Width cannot be less than {MinWidth}");
+            AssertionConcern.AssertArgumentGreaterThan(depth, MinDepth, $"Depth cannot be less than {MinDepth}");
 
             Height = height;
             Width = width;
             Depth = depth;
         }
 
-        public string FormattedDescription()
+        private string FormatDescripton()
         {
             return $"WxHxD: {Width} x {Height} x {Depth}";
         }
 
         public override string ToString()
         {
-            return FormattedDescription();
+            return FormatDescripton();
         }
     }
 }
